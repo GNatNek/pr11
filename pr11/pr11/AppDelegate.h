5e8e316cb29c33ff6a46951959fad8f3a220c415
GNatNek <htweffendt@gmail.com>
2016-08-30 21:24:29 +0700

    first commit

---
+++ pr11/pr11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pr11
+//
+//  Created by Hartawan Effendy on 8/30/16.
+//  Copyright © 2016 Hartawan Effendy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pr11/pr11/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pr11
+//
+//  Created by Hartawan Effendy on 8/30/16.
+//  Copyright © 2016 Hartawan Effendy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
